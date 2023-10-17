3edb2ee9c9d1651397c098a4ea243ca8e504872c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-17 07:39:27 +0300

    Added url for product createion

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.product_alt_view),
-    path('<int:pk>/update/',views.product_update_view),
-    path('<int:pk>/delete/',views.product_delete_view),
-    path('<int:pk>/',views.product_alt_view),
+    path('create/', views.product_create_view),
+    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/delete/', views.product_delete_view),
+    path('<int:pk>/', views.product_alt_view),
 ]
