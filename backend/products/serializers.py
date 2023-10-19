193d2b8c86124fd12d05536245c03467bc1a86ee
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-19 08:19:48 +0300

    Minor changes

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -6,6 +6,7 @@ from .validators import unique_product_title
 
 class ProductSerializer(serializers.ModelSerializer):
     title = serializers.CharField(validators=[unique_product_title])
+
     class Meta:
         model = Product
         fields = [
@@ -15,9 +16,3 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'sale_price'
         ]
-
-    def validate_title(self, value):
-        qs = Product.objects.filter(title=value)
-        if qs.exists():
-            raise serializers.ValidationError(f"{value} is already a product name")
-        return value
