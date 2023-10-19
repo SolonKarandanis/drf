cbcad4b8917a8d27dda2a9bd4bd5a0f481098d63
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-19 07:37:16 +0300

    Added names in urls

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_alt_view),
-    path('create/', views.product_create_view),
-    path('<int:pk>/update/', views.product_update_view),
-    path('<int:pk>/delete/', views.product_delete_view),
-    path('<int:pk>/', views.product_alt_view),
+    path('', views.product_alt_view, name='product-list'),
+    path('create/', views.product_create_view, name='product-create'),
+    path('<int:pk>/update/', views.product_update_view, name='product-update'),
+    path('<int:pk>/delete/', views.product_delete_view, name='product-delete'),
+    path('<int:pk>/', views.product_alt_view, name='product-detail'),
 ]
