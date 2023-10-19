fb4830055648b63bd94f960d5bf1a15971161802
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-19 08:33:52 +0300

    Added User relation to product

---
+++ backend/auth/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserPublicSerializer(serializers.Serializer):
+    username = serializers.CharField(read_only=True)
+    this_is_not_real = serializers.CharField(read_only=True)
+    id = serializers.IntegerField(read_only=True)
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,14 +2,16 @@ from rest_framework import serializers
 
 from .models import Product
 from .validators import unique_product_title
+from auth.serializers import UserPublicSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
     title = serializers.CharField(validators=[unique_product_title])
-
+    owner = UserPublicSerializer(source='user', read_only=True)
     class Meta:
         model = Product
         fields = [
+            'owner',
             'id',
             'title',
             'content',
