b2fb7e9da48fa2d63a7728c69d14d309803e704b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-24 08:36:34 +0300

    Added auth in method

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,7 +1,8 @@
 import logging
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserSerializer
 
@@ -11,6 +12,7 @@ logger = logging.getLogger('django')
 # Create your views here.
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_all_users(request):
     queryset = User.objects.all()
     data = UserSerializer(queryset, many=True).data
