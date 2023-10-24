379192dbf21e5b6a3a9e90b4e1fe430b3e5f71ca
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-24 14:32:20 +0300

    Created Cart model

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+
+class CartManager(models.Manager):
+    def create_cart(self, user):
+        cart = self.create(user=user)
+        return cart
+
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    objects = CartManager()
