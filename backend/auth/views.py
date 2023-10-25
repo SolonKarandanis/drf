65b0acb8d45052ebcc3aacbe93b9cb0b873e3040
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-25 07:42:32 +0300

    Removed unused imports

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -4,7 +4,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-# from rest_framework_simplejwt.authentication import
 from .models import User
 from .serializers import UserSerializer, CreateUserSerializer
 
