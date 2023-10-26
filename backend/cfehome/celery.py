129cae3063fcdbb2cd561f99425b670dfcd973b5
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-26 09:29:11 +0300

    Created celery file

---
+++ backend/cfehome/celery.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
+app = Celery('cfehome')
+app.conf.enable_utc = False
+app.conf.update(timezone='Europe/Athens')
+app.config_from_object(settings, namespace='CELERY')
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request}')
