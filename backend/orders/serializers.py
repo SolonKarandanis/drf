967da9b830c420e60482841b6dcadb760fffaa2d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-11-11 11:24:27 +0200

    Created Order Serializers

---
+++ backend/orders/serializers.py
@@ -0,0 +1,35 @@
+import logging
+
+from rest_framework import serializers
+from .models import Order, OrderItem
+from cfehome.serializers import ModelPaginationSerializer
+
+logger = logging.getLogger('django')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+        ]
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = [
+            'id',
+        ]
+
+
+class PaginatedOrderSerializer(ModelPaginationSerializer):
+    """
+    Serializes page objects of product querysets.
+    """
+
+    def __init__(self, data, request):
+        super().__init__(data, request)
+        serializer = OrderSerializer(data, many=True)
+        self.data = {'count': self.page_data.get('count'), 'previous': self.page_data.get('previous'),
+                     'next': self.page_data.get('next'), 'data': serializer.data}
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -5,5 +5,27 @@ from rest_framework import status
 
 import logging
 from .models import Order
+
 # Create your views here.
 logger = logging.getLogger('django')
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_orders(request):
+    logged_in_user = request.user
+    logger.info(f'logged_in_user: {logged_in_user}')
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_order(request, pk):
+    logged_in_user = request.user
+    logger.info(f'logged_in_user: {logged_in_user}')
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def place_order(request):
+    logged_in_user = request.user
+    logger.info(f'logged_in_user: {logged_in_user}')
