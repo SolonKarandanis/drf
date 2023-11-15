b8130fb893c98773d1b61dc5b487a4cb08e99cf8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-11-15 07:51:45 +0200

    Updated auth model

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -58,23 +58,8 @@ class UserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class UserType(models.Model):
-    CUSTOMER = 1
-    SELLER = 2
-    TYPE_CHOICES = (
-        (SELLER, 'Seller'),
-        (CUSTOMER, 'Customer')
-    )
-
-    id = models.PositiveSmallIntegerField(choices=TYPE_CHOICES, primary_key=True)
-
-    def __str__(self):
-        return self.get_id_display()
-
-
 # Create your models here.
 class User(AbstractUser):
-    user_type = models.ManyToManyField(UserType)
     created_date = models.DateTimeField(auto_now_add=True, null=False)
     updated_date = models.DateTimeField(auto_now=True, null=False)
     email = models.EmailField(_("email address"), unique=True)
