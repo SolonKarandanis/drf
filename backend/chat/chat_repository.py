f9cdf7eb713d91966dc2b6b7bbcd667d1f633c41
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-11-29 09:43:11 +0200

    Added chat repo

---
+++ backend/chat/chat_repository.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from .models import Room, Message
+
+User = settings.AUTH_USER_MODEL
+
+
+class ChatRepository:
+
+    def join_room(self, user: User):
+        pass
+
+    def leave_room(self, user: User):
+        pass
+
+    def get_room_online_count(self):
+        pass
+    
