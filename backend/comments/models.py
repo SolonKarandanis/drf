8a286b521b0b6cb53e68e1108a2756f2313d0507
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-12-04 09:16:15 +0200

    Created  Comment Schema

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db.models import Model, DateTimeField, TextField, ForeignKey, CASCADE, PositiveSmallIntegerField
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
+class Comment(Model):
+    content = TextField()
+    content_type = ForeignKey(ContentType, on_delete=CASCADE)
+    object_id = PositiveSmallIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    date_created = DateTimeField(auto_now_add=True, null=False)
