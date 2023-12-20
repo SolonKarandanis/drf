11b9e8a0f430bb2c7e5a0809e6cb28e44a616351
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-12-20 09:43:44 +0200

    Adding tests

--- backend/orders/tests.py
+++ backend/orders/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from rest_framework import status
+
 
 # Create your tests here.
+class URLTests(TestCase):
+
+    def test_get_user_orders(self):
+        response = self.client.get('/orders/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
