65a42baf17cc0ccd9783e042aed60480f89eddaf
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-01-02 10:53:34 +0200

    Created Image renderers and serializers

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import datetime
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -248,3 +249,6 @@ CACHES = {
         "KEY_PREFIX": "drf"
     }
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
