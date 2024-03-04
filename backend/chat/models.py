3a0a2833566922414d73c6349b1e077e68c7f297
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-04 08:23:59 +0200

    Chat model improvements

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+import uuid
 
 # Create your models here.
 User = settings.AUTH_USER_MODEL
@@ -8,16 +9,22 @@ User = settings.AUTH_USER_MODEL
 class Room(models.Model):
     name = models.CharField(max_length=128)
     online = models.ManyToManyField(to=User, blank=True)
+    uuid = models.UUIDField(default=uuid.uuid4())
 
     def get_online_count(self):
         return self.online.count()
 
     def join(self, user):
+        """A helper function to add a user to a Room and create an event object"""
         self.online.add(user)
+        self.event_set.create(type="Join", user=user)
         self.save()
 
     def leave(self, user):
+        """An helper function to remove users from group members when they \
+                leave the group and create an event for the timestamp the user left the group"""
         self.online.remove(user)
+        self.event_set.create(type="Left", user=user)
         self.save()
 
     def __str__(self):
@@ -32,3 +39,25 @@ class Message(models.Model):
 
     def __str__(self):
         return f'{self.user.username}: {self.content} [{self.timestamp}]'
+
+
+class Event(models.Model):
+    """
+    A model that holds all events related to a group like when a user joins the group or leaves.
+    """
+    CHOICES = [
+        ("Join", "join"),
+        ("Left", "left")
+    ]
+    type = models.CharField(choices=CHOICES, max_length=10)
+    description = models.CharField(max_length=50, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.description = f"{self.user} {self.type} the {self.room.name} room"
+        super().save(*args, kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.description}"
