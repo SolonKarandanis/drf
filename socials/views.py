25c8e4dbfe63e51139a90db32ba49585bf89558a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-30 19:24:16 +0300

    Created find_users_socials view

--- socials/views.py
+++ socials/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from socials.serializers import SocialUserSerializer
+from socials.social_service import SocialService
+
+social_service = SocialService()
+
 
 # Create your views here.
+@api_view(['GET'])
+def find_users_socials(request):
+    user_id = request.GET.get('userId')
+    if user_id is None:
+        return Response({"userId": "Required"}, status=status.HTTP_400_BAD_REQUEST)
+    queryset = social_service.find_users_socials(user_id)
+    data = SocialUserSerializer(queryset).data
+    return Response(data)
