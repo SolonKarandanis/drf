00659fe54aeadbf2de361565e3068daae3c839ad
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-30 19:14:04 +0300

    Serializers

--- socials/serializers.py
+++ socials/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 
-from socials.models import SocialUser
+from socials.models import SocialUser, Social
+
+
+class SocialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Social
+        fields = [
+            'name',
+            'icon',
+        ]
 
 
 class SocialUserSerializer(serializers.ModelSerializer):
     userId = serializers.IntegerField(source='user_id', read_only=True)
     socialId = serializers.IntegerField(source='social_id', read_only=True)
+    socialName = serializers.CharField(source="social__name")
+    socialIcon = serializers.CharField(source="social__icon")
 
     class Meta:
         model = SocialUser
@@ -14,4 +25,6 @@ class SocialUserSerializer(serializers.ModelSerializer):
             'userId',
             'socialId',
             'url',
+            'socialName',
+            'socialIcon'
         ]
