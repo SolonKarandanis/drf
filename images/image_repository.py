44acf2286faef6b138d30513692a6a9fd9a53320
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-01 14:55:41 +0300

    Created image repository

--- images/image_repository.py
+++ images/image_repository.py
@@ -1,13 +1,22 @@
+from typing import List
+
 from images.models import Images
+from django.conf import settings
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
 
 
 class ImageRepository:
 
-    def find_image_by_id(self, image_id: int):
+    def find_image_by_id(self, image_id: int) -> Images:
         return Images.objects.get(pk=image_id)
 
-    def find_user_profile_image(self, user_id: int):
-        pass
+    def find_image_by_object_id(self, object_id: int) -> List[Images]:
+        return Images.objects.filter(object_id=object_id)
 
-    def upload_image(self):
-        pass
+    def upload_profile_image(self, title: str, alt: str, user: User, logged_in_user: User) -> None:
+        Images.objects.create(title=title, alt=alt, content_object=user, user=logged_in_user)
+
+    def upload_product_image(self, title: str, alt: str, product: Product, logged_in_user: User) -> None:
+        Images.objects.create(title=title, alt=alt, content_object=product, user=logged_in_user)
