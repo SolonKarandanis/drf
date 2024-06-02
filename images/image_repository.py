8b4585ca4cf8266987585f74ea7ead08c970deee
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-02 17:06:57 +0300

    Implementing upload user profile

--- images/image_repository.py
+++ images/image_repository.py
@@ -18,7 +18,10 @@ class ImageRepository:
         return Images.objects.filter(object_id=object_id)
 
     def upload_profile_image(self, image: InMemoryUploadedFile, title: str, alt: str, user: User, logged_in_user: User) -> None:
-        Images.objects.create(title=title, alt=alt, image=image, content_object=user, user=logged_in_user)
+        images = Images(title=title, alt=alt, image=image, content_object=user)
+        images.save()
+        images.user.set(logged_in_user)
+        images.save()
 
     def upload_product_image(self, image: InMemoryUploadedFile, title: str, alt: str, product: Product, logged_in_user: User) -> None:
-        Images.objects.create(title=title, alt=alt, image=image, ontent_object=product, user=logged_in_user)
+        Images.objects.create(title=title, alt=alt, image=image, content_object=product, user=logged_in_user)
