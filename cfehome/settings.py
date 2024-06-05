fcdf7e15db48d65065bba99629ffca109ddf1a1c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-05 16:15:06 +0300

    Added migrations

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -69,7 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware"
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    'pghistory.middleware.HistoryMiddleware'
 ]
 
 ROOT_URLCONF = 'cfehome.urls'
