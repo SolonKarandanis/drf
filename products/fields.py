5d3dc7d92808d96cb107c86cf31ba2c93aed230d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-21 10:00:49 +0300

    Created OrderField

--- products/fields.py
+++ products/fields.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 
@@ -8,3 +9,18 @@ class ProductIDField(models.CharField):
         kwargs['max_length'] = 10
         kwargs['unique'] = True
         super().__init__(*args, **kwargs)
+
+
+class OrderField(models.PositiveIntegerField):
+
+    def pre_save(self, model_instance, add):
+        if getattr(model_instance, self.attname) is None:
+            try:
+                obj = self.model.objects.latest(self.attname)
+                value = obj.order + 1
+            except ObjectDoesNotExist:
+                value = 1
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super().pre_save(model_instance, add)
