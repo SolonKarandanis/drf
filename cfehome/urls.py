cfa2aabc51b52148fc7ca8b7632cf06f9ece892c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-04 09:15:16 +0300

    User image

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('api/orders/', include('orders.urls')),
     path('api/socials/', include('socials.urls')),
     # path('api/v2/', include('cfehome.routers'))
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
