e36a0d16e4832bc1098cca9e8d53b58e2283971d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-08-22 08:46:28 +0300

    Creating billing info views

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import logging
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+
+logger = logging.getLogger('django')
+
 
 # Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_users_billing_info(request):
+    pass
