378b31a05e5f683f10c708290171632a129a8386
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-21 10:42:02 +0300

    Implementing card functionality

--- billing/card_repository.py
+++ billing/card_repository.py
@@ -1,6 +1,7 @@
 import logging
 from typing import List
 
+from auth.models import User
 from billing.models import Card
 
 logger = logging.getLogger('django')
@@ -9,5 +10,8 @@ logger = logging.getLogger('django')
 class CardRepository:
 
     def find_user_cards(self) -> List[Card]:
+        pass
+
+    def create_user_card(self, logged_in_user: User) -> None:
         pass
 
