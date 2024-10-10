3a7bdae2416883919fcfbf9084389f22ffdf7f27
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-10 11:32:36 +0300

    Removed views

--- products/views.py
+++ products/views.py
@@ -4,12 +4,10 @@ from .serializers import ProductSerializer, CreateProductSerializer, PaginatedPr
     PostProductComment
 from .models import Product
 from rest_framework import status
-from rest_framework import generics, mixins, permissions, authentication
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.core.paginator import Paginator, EmptyPage
-from auth.mixins import StaffEditorPermissionMixin, UserQuerySetMixin
 from .product_service import ProductService
 
 product_service = ProductService()
@@ -82,83 +80,3 @@ def post_product_comment(request):
         return Response(response, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class ProductMixinView(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    generics.GenericAPIView
-):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'pk'
-
-    def get(self, request, *args, **kwargs):
-        pk = kwargs.get("pk")
-        if pk is not None:
-            return self.retrieve(request, *args, **kwargs)
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-product_mixin_view = ProductMixinView.as_view()
-
-
-class ProductListCreateAPIView(generics.ListCreateAPIView, StaffEditorPermissionMixin, UserQuerySetMixin):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
-
-
-product_list_create_view = ProductListCreateAPIView.as_view()
-
-
-class ProductCreateAPIView(generics.CreateAPIView, StaffEditorPermissionMixin):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-    def perform_create(self, serializer):
-        title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content') or None
-        if content is None:
-            content = title
-        serializer.save(content=content)
-
-
-product_create_view = ProductCreateAPIView.as_view()
-
-
-class ProductDetailAPIView(generics.RetrieveAPIView, StaffEditorPermissionMixin):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-
-product_detail_view = ProductDetailAPIView.as_view()
-
-
-class ProductUpdateAPIView(generics.UpdateAPIView, StaffEditorPermissionMixin):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'pk'
-
-    def perform_update(self, serializer):
-        instance = serializer.save()
-        if not instance.content:
-            instance.content = instance.title
-
-
-product_update_view = ProductUpdateAPIView.as_view()
-
-
-class ProductDeleteAPIView(generics.DestroyAPIView, StaffEditorPermissionMixin):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'pk'
-
-    def perform_destroy(self, instance):
-        super().perform_destroy(instance)
-
-
-product_delete_view = ProductDeleteAPIView.as_view()
