fec47ab0e68d2f9872230f3de980eca0c5fe7d4d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-21 08:37:24 +0300

    Added search GeneratedField

--- products/models.py
+++ products/models.py
@@ -120,13 +120,13 @@ class Product(Model):
     inventory = IntegerField(blank=True, null=True)
     number_sold = IntegerField(blank=True, null=True)
     uuid = UUIDField(default=uuid.uuid4())
-    # search = GeneratedField(
-    #     db_persist=True,
-    #     expression=SearchVector(
-    #         "sku", "title", "content", config="english"
-    #     ),
-    #     output_field=SearchVectorField(),
-    # )
+    search = GeneratedField(
+        db_persist=True,
+        expression=SearchVector(
+            "sku", "title", "content", config="english"
+        ),
+        output_field=SearchVectorField(),
+    )
     published_date = DateTimeField(auto_now_add=True, null=True)
     publish_status = CharField(max_length=40, choices=ProductPublishedStatus.choices,
                                db_default=ProductPublishedStatus.PUBLISHED)
