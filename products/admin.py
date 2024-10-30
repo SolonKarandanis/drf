94655e16519a9d3ae503ce7c36dedcbab4ed2b02
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-30 15:11:46 +0200

    Added models in admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Brand, Category, Attribute, AttributeOptions, ProductAttributeValues
+from .models import Product, Brand, Category, Attribute, AttributeOptions, ProductAttributeValues, Discount, \
+    ProductsDiscount
 
 admin.site.register(Product)
 admin.site.register(Brand)
@@ -9,3 +10,5 @@ admin.site.register(Category)
 admin.site.register(Attribute)
 admin.site.register(AttributeOptions)
 admin.site.register(ProductAttributeValues)
+admin.site.register(Discount)
+admin.site.register(ProductsDiscount)
