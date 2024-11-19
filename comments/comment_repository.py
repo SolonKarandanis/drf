ef26d8a1cfbf0af8343b5f08ddbd01a7162f6241
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-19 11:51:11 +0200

    Changes in comment repo

--- comments/comment_repository.py
+++ comments/comment_repository.py
@@ -11,11 +11,17 @@ class CommentRepository:
     def create_order_comment(self, comment: str, order: Order, logged_in_user: User) -> None:
         username = logged_in_user.username
         user_email = logged_in_user.email
+        first_name = logged_in_user.first_name
+        last_name = logged_in_user.last_name
         Comment.objects.create(content=comment, content_object=order, user=logged_in_user,
-                               user_username=username, user_email=user_email)
+                               user_username=username, user_email=user_email, user_first_name=first_name,
+                               user_last_name=last_name)
 
     def create_product_comment(self, comment: str, product: Product, logged_in_user: User) -> None:
         username = logged_in_user.username
         user_email = logged_in_user.email
+        first_name = logged_in_user.first_name
+        last_name = logged_in_user.last_name
         Comment.objects.create(content=comment, content_object=product, user=logged_in_user,
-                               user_username=username, user_email=user_email)
+                               user_username=username, user_email=user_email, user_first_name=first_name,
+                               user_last_name=last_name)
