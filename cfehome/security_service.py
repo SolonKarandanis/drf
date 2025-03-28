cba462ee4da5a6f3b88d745e6b9e7c0aa2b11a81
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-03-28 07:07:30 +0200

    Implementing security_service

--- cfehome/security_service.py
+++ cfehome/security_service.py
@@ -2,11 +2,13 @@ import logging
 
 from auth.group_service import GroupService
 from auth.user_service import UserService
+from cart.cart_service import CartService
 
 logger = logging.getLogger('django')
 
 user_service = UserService()
 group_service = GroupService()
+cart_service = CartService()
 
 class SecurityService:
     pass
