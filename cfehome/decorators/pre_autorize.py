796ebf8f7131ab729715b3af4e01ea9da59f0d35
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-03-28 07:31:48 +0200

    Implementing pre_authorize

--- cfehome/decorators/pre_autorize.py
+++ cfehome/decorators/pre_autorize.py
@@ -20,16 +20,19 @@ def pre_authorize(value: str):
         def wrapped(*args, **kwargs):
             is_authorized = False
             request: Request = args[0]
-            has_permission_pattern = re.compile(r"(hasPermission)(.+)", re.IGNORECASE)
-            has_permission_match = has_permission_pattern.match(value)
-            if has_permission_match:
-                permission = has_permission_match.group()[14:-1]
-                is_authorized = _check_user_permission(request, permission)
-            has_role_pattern = re.compile(r"(hasRole)(.+)", re.IGNORECASE)
-            has_role_match = has_role_pattern.match(value)
-            if has_role_match:
-                role = has_role_match.group()[8:-1]
-                is_authorized = _check_user_role(request, role)
+            parts_split_by_and = value.split('&&')
+            for part in parts_split_by_and:
+                trimmed_part = part.strip()
+                has_permission_pattern = re.compile(r"(hasPermission)(.+)", re.IGNORECASE)
+                has_permission_match = has_permission_pattern.match(trimmed_part)
+                if has_permission_match:
+                    permission = has_permission_match.group()[14:-1]
+                    is_authorized = _check_user_permission(request, permission)
+                has_role_pattern = re.compile(r"(hasRole)(.+)", re.IGNORECASE)
+                has_role_match = has_role_pattern.match(trimmed_part)
+                if has_role_match:
+                    role = has_role_match.group()[8:-1]
+                    is_authorized = _check_user_role(request, role)
             if not is_authorized:
                 return Response(status=status.HTTP_401_UNAUTHORIZED)
             response = function(*args, **kwargs)
--- cfehome/security_service.py
+++ cfehome/security_service.py
@@ -1,14 +1,25 @@
 import logging
+from typing import List
 
 from auth.group_service import GroupService
 from auth.user_service import UserService
 from cart.cart_service import CartService
+from products.product_service import ProductService
 
 logger = logging.getLogger('django')
 
 user_service = UserService()
 group_service = GroupService()
 cart_service = CartService()
+product_service = ProductService()
+
 
 class SecurityService:
-    pass
+
+    def is_product_mine(self, product_item_id: int) -> bool:
+        return True
+
+    def are_cart_items_mine(self, cart_item_ids: List[int]) -> bool:
+        return True
+
+
