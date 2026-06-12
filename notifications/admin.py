b0c763f590cb4a78d631244801d0e111dc40c660
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-06-12 07:14:32 +0300

    Implementing issues https://github.com/SolonKarandanis/drf/issues/3 and https://github.com/SolonKarandanis/drf/issues/4

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import BroadcastNotification
+from .models import BroadcastNotification, NotificationEvent
+
 admin.site.register(BroadcastNotification)
+admin.site.register(NotificationEvent)
